0f30ec3b3433fc4d8df82b094bc0525ac4728201
Huynh <user@users-iMac.local>
2019-09-26 10:40:17 +0700

    update

--- src/components/app.vue
+++ src/components/app.vue
@@ -11,23 +11,42 @@
 
                         <div class="form-container">
                             <div class="repeat-control" v-for="(item_data, index) in form_data" :key="item_data">
+                                <button v-if="index > 0" type="button" class="remove" title="remove" @click="remove(item_data)">
+                                    <svg x="0px" y="0px" viewBox="0 0 459 459" style="enable-background:new 0 0 459 459;" xml:space="preserve"> <g> <g> <path d="M229.5,0C102.751,0,0,102.751,0,229.5S102.751,459,229.5,459S459,356.249,459,229.5S356.249,0,229.5,0z M307.105,271.629 c9.797,9.797,9.797,25.68,0,35.477c-4.898,4.898-11.318,7.347-17.738,7.347c-6.42,0-12.84-2.449-17.738-7.347L229.5,264.977 l-42.128,42.129c-4.898,4.898-11.318,7.347-17.738,7.347c-6.42,0-12.84-2.449-17.738-7.347c-9.797-9.796-9.797-25.68,0-35.477 l42.129-42.129l-42.129-42.129c-9.797-9.797-9.797-25.68,0-35.477s25.68-9.797,35.477,0l42.128,42.129l42.128-42.129 c9.797-9.797,25.68-9.797,35.477,0c9.797,9.796,9.797,25.68,0,35.477l-42.13,42.129L307.105,271.629z"/> </g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </svg>
+                                </button>
+
                                 <div class="field" v-for="field in form" :key="field">
-                                    <button v-if="index > 0" type="button" class="remove" title="remove" @click="remove(item_data)">
-                                        <svg x="0px" y="0px" viewBox="0 0 459 459" style="enable-background:new 0 0 459 459;" xml:space="preserve"> <g> <g> <path d="M229.5,0C102.751,0,0,102.751,0,229.5S102.751,459,229.5,459S459,356.249,459,229.5S356.249,0,229.5,0z M307.105,271.629 c9.797,9.797,9.797,25.68,0,35.477c-4.898,4.898-11.318,7.347-17.738,7.347c-6.42,0-12.84-2.449-17.738-7.347L229.5,264.977 l-42.128,42.129c-4.898,4.898-11.318,7.347-17.738,7.347c-6.42,0-12.84-2.449-17.738-7.347c-9.797-9.796-9.797-25.68,0-35.477 l42.129-42.129l-42.129-42.129c-9.797-9.797-9.797-25.68,0-35.477s25.68-9.797,35.477,0l42.128,42.129l42.128-42.129 c9.797-9.797,25.68-9.797,35.477,0c9.797,9.796,9.797,25.68,0,35.477l-42.13,42.129L307.105,271.629z"/> </g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </svg>
-                                    </button>
                                     <label class="label">{{ field.label }}</label>
                                     <div class="control">
                                         <input class="input" type="text" v-model="item_data[field.name]">
                                     </div>
                                 </div>
                             </div>
-                            <button type="button" name="button" @click="add_more">Add More</button>
+                            <button type="button" name="button" class="button is-primary btn-add-question" @click="add_more">Add Question</button>
                         </div> <!-- .form-container -->
 
                     </div>
 
                     <div class="column">
-<pre>{{ form_data }}</pre>
+                        <div class="tools">
+                            <p class="buttons">
+                                <a class="button is-primary" @click="copy()">
+                                    <span class="icon"><i class="ion-ios-copy"></i></span>
+                                    <span>Copy</span>
+                                </a>
+                                <a class="Disabled button is-success" disabled>
+                                    <span class="icon is-small"><i class="ion-social-codepen"></i></span>
+                                    <span>Test</span>
+                                </a>
+                                <a class="button is-danger is-outlined" @click="reset()">
+                                    <span>Reset</span>
+                                    <span class="icon is-small"><i class="ion-ios-refresh"></i></span>
+                                </a>
+                            </p>
+                        </div>
+<pre id="mockup">
+&lt;script type="application/ld+json"&gt;<div v-html="mockup_html"></div>&lt;/script&gt;
+</pre>
                     </div>
 
                 </div>
@@ -62,6 +81,28 @@
                 ],
             }
         },
+        computed: {
+            mockup_html () {
+                var output = [];
+
+                this.form_data.forEach( function( item, index ) {
+                    output.push( {
+                        "@type": "Question",
+                        "name": `<span class='hight-line'>${item.question}</span>`,
+                        "acceptedAnswer": {
+                            "@type": "Answer",
+                            "text": `<span class='hight-line'>${item.answer}</span>`
+                        }
+                    } );
+                } )
+
+                return {
+                    "@context": "https://schema.org",
+                    "@type": "FAQPage",
+                    "mainEntity": output,
+                };
+            }
+        },
         methods: {
             add_more: function() {
                 this.form_data.push( {
@@ -76,6 +117,26 @@
                 } );
 
                 this.form_data = new_data;
+            },
+            copy: function() {
+                var dummy = document.createElement('textarea');
+                document.body.appendChild(dummy);
+                dummy.value = document.getElementById('mockup').innerHTML.replaceArray(
+                    ["&lt;", "&gt;", "<span class=\"hight-line\">", "<span class='hight-line'>", "</span>", "<div>", "</div>"],
+                    ["<", ">", "", "", "", "", ""]
+                );
+
+                dummy.select();
+                document.execCommand("copy");
+                document.body.removeChild(dummy);
+            },
+            reset: function() {
+                this.form_data = [
+                    {
+                        'question': '',
+                        'answer': '',
+                    }
+                ];
             }
         },
     }
--- src/main.js
+++ src/main.js
@@ -8,6 +8,17 @@
 import Vue from 'vue';
 import App from './components/app.vue';
 
+String.prototype.replaceArray = function(find, replace) {
+    var replaceString = this;
+    var regex;
+    
+    for (var i = 0; i < find.length; i++) {
+        regex = new RegExp(find[i], "g");
+        replaceString = replaceString.replace(regex, replace[i]);
+    }
+    return replaceString;
+};
+
 var app = new Vue({
    el: '#app',
    render: h => h(App),
